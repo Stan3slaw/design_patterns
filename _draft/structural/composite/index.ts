9a75e34238f0574e77d4d43085012438b948210c
Stanislav Pastushok <stan3slaw@gmail.com>
2022-06-30 09:53:25 +0300

    added composite design pattern to draft

---
+++ _draft/structural/composite/index.ts
@@ -0,0 +1,75 @@
+abstract class Component {
+  protected parent!: Component | null;
+
+  public setParent(parent: Component | null) {
+    this.parent = parent;
+  }
+
+  public getParent(): Component | null {
+    return this.parent;
+  }
+
+  public isComposite(): boolean {
+    return false;
+  }
+
+  public abstract operation(): string;
+}
+
+class Leaf extends Component {
+  public operation(): string {
+    return 'Leaf';
+  }
+}
+
+class Composite extends Component {
+  protected children: Component[] = [];
+
+  public add(component: Component): void {
+    this.children.push(component);
+    component.setParent(this);
+  }
+
+  public remove(component: Component): void {
+    const componentIndex = this.children.indexOf(component);
+    this.children.splice(componentIndex, 1);
+
+    component.setParent(null);
+  }
+
+  public isComposite(): boolean {
+    return true;
+  }
+
+  public operation(): string {
+    const results = [];
+    for (const child of this.children) {
+      results.push(child.operation());
+    }
+
+    return `Branch(${results.join('+')})`;
+  }
+}
+
+function print(component: Component) {
+  console.log(`RESULT: ${component.operation()}`);
+}
+
+function clientCode() {
+  const simple = new Leaf();
+  console.log("Client: I've got a simple component:");
+  print(simple);
+
+  const tree = new Composite();
+  const branch1 = new Composite();
+  branch1.add(new Leaf());
+  branch1.add(new Leaf());
+  const branch2 = new Composite();
+  branch2.add(new Leaf());
+  tree.add(branch1);
+  tree.add(branch2);
+  console.log("Client: Now I've got a composite tree:");
+  print(tree);
+}
+
+clientCode();
